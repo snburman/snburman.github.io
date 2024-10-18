9b86db3ad99acf7173297b33c7665055248633a7
Sean Burman <sb9443353@gmail.com>
2024-10-17 19:37:46 -0600

    Deploy website - based on 55f772764edf5d33d93a17baa38e67bb43811088

---
+++ assets/js/bc8e7d5a.e72d34c9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[596],{1114:e=>{e.exports=JSON.parse('{"title":"Reference","slug":"/category/reference","permalink":"/docs/category/reference","navigation":{"previous":{"title":"Cache","permalink":"/docs/fncmp/tutorial/cache"},"next":{"title":"pkg.go.dev","permalink":"/docs/fncmp/reference/intro"}}}')}}]);
